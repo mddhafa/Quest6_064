76a47d5ae9fa2010c1aff0001a7f7f9d2b75440b
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-11-20 18:16:14 +0700

    membuat fungsi matakuliah

--- app/src/main/java/com/example/minggu7/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/minggu7/ui/viewmodel/RencanaStudyViewModel.kt
@@ -5,10 +5,15 @@ import com.example.minggu7.model.RencanaStudy
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudy())
     val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
 
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan)
+        }
+    }
 
 }

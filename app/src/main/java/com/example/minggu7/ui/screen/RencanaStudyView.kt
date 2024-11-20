e7430876cbb7fe835ec3956d45340d50b63cbfd1
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-11-20 17:13:37 +0700

    Membuat Box lalu menambahkan Column didalamnya

--- app/src/main/java/com/example/minggu7/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/minggu7/ui/screen/RencanaStudyView.kt
@@ -2,6 +2,7 @@ package com.example.minggu7.ui.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -75,6 +77,21 @@ fun RencanaStudyView(
                     fontSize = 12.sp,
                     color = Color.White
                 )
+            }
+            Box(
+                modifier = Modifier
+                    .background(color = Color.White,
+                        shape = RoundedCornerShape(
+                            topEnd = 15.dp,
+                            topStart = 15.dp
+                        )
+                    )
+                    .fillMaxSize(),
+            ){
+                Column () {
+
+                }
+
             }
         }
     }

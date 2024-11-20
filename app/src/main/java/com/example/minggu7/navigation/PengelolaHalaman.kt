d9b8c21fdbcfdac2f76893e85560083f2eed97d2
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-11-21 02:15:11 +0700

    membuat variabel mahasiswauiState dan NavHost

--- app/src/main/java/com/example/minggu7/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/minggu7/navigation/PengelolaHalaman.kt
@@ -1,8 +1,11 @@
 package com.example.minggu7.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 //import androidx.navigation.NavHostController
 import com.example.minggu7.ui.viewmodel.RencanaStudyViewModel
@@ -22,5 +25,12 @@ fun MahasiswaApp(
     krsViewModel: RencanaStudyViewModel = viewmodel(),
     navController: NavHostController = rememberNavController()
 ){
+    val mahasiswauiState = mahasiswaviewmodel.mahasiswaUiState.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ){
 
+    }
 }

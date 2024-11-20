3fcb6415f08c085b6ba5dcc1265548d139631bbf
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-11-20 17:04:26 +0700

    Membuat row lalu menambahkan gambar

--- app/src/main/java/com/example/minggu7/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/minggu7/ui/screen/RencanaStudyView.kt
@@ -1,15 +1,25 @@
 package com.example.minggu7.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.minggu7.R
 import com.example.minggu7.model.RencanaStudy
 import com.example.minggu7.model.mahasiswa
@@ -32,5 +42,20 @@ fun RencanaStudyView(
         modifier = Modifier
             .fillMaxSize()
             .background(color = colorResource(R.color.primary))
-    ){  }
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(
+                painter = painterResource(R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+        }
+    }
 }

f19a75fa77faa2417d7057cb34fe0c2ba1ac76c0
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-11-20 19:14:17 +0700

    membuat package navigator dan fungsi pengelolahalaman

--- app/src/main/java/com/example/minggu7/ui/viewmodel/mahasiswaviewmodel.kt
+++ app/src/main/java/com/example/minggu7/ui/viewmodel/mahasiswaviewmodel.kt
@@ -2,20 +2,23 @@ package com.example.minggu7.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.minggu7.model.mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class mahasiswaviewmodel : ViewModel() {
 
-    //r
+    //requ
     private val mahasiswaStateUI =
-        mutableStateFlow(mahasiswa())
+        MutableStateFlow(mahasiswa())
 
     val mahasiswaUiState:
             StateFlow<mahasiswa> =
         mahasiswaStateUI.asStateFlow()
 
     fun saveDataMahasiswa(ls: MutableList<String>){
-        mahasiswaStateUI.update { statusSaatIni ->
+        mahasiswaStateUI.update{ statusSaatIni ->
             statusSaatIni.copy(
                 nim = ls[0],
                 nama = ls[1],
